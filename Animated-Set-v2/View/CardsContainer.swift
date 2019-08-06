9d331e2643ffe12dc6becc268d9ec0e40c2a3819
saman <abdallaelsaman@gmail.com>
2019-08-06 22:09:46 +0200

    cardsContainer factorization

--- Animated-Set-v2/View/CardsContainer.swift
+++ Animated-Set-v2/View/CardsContainer.swift
@@ -14,137 +14,103 @@ class CardsContainer: UIView {
     
     // buffer for doing shuffling 
     var playingCardViews = [PlayingCardView]()
-    
+    var grid = Grid(layout: .aspectRatio(0.662))
     var dickFrame: CGRect?
     var pileFrame: CGRect?
-    
+    var animationState = AnimationState.begining
+
     enum AnimationState {
         case begining, dealt, changingBounds, dealing
     }
-
     
-    var animationState = AnimationState.begining
-    
-    override func draw(_ rect: CGRect) {
-        var grid = Grid(layout: .aspectRatio(0.662))
+    fileprivate func gridSetup() {
         grid.cellCount = playingCardViews.count
         grid.frame = bounds
-        
+    }
+    
+    fileprivate func playingCardViewSetup(_ index: Int) {
+        playingCardViews[index].frame.origin = dickFrame!.origin.offsetBy(dx: -self.frame.origin.x, dy: -self.frame.origin.y)
+        playingCardViews[index].frame.size = dickFrame!.size
+        playingCardViews[index].layer.cornerRadius = 6
+        addSubview(playingCardViews[index])
+    }
+    
+    fileprivate func layoutNewsubviews(_ index: Int) {
+        if let viewFrame = grid[index] {
+            let view = playingCardViews[index]
+            let padding = viewFrame.height * Constants.paddingRatio
+            let newViewFrame = viewFrame.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
+            UIViewPropertyAnimator.runningPropertyAnimator(
+                withDuration: 0.5,
+                delay: 0,
+                options: [.allowUserInteraction],
+                animations: {
+                    view.frame = newViewFrame
+            },
+                completion: nil)
+            addSubview(view)
+        }
+    }
+    
+    fileprivate func deal3moreAnimation(_ index: Int, with delayFactor: Double) {
+        UIViewPropertyAnimator.runningPropertyAnimator(
+            withDuration: 0.75,
+            delay: 0.375 * Double(delayFactor),
+            options: .curveEaseInOut,
+            animations: {
+                if let viewFrame = self.grid[index] {
+                    let padding = viewFrame.height * Constants.paddingRatio
+                    let newViewFrame = viewFrame.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
+                    self.playingCardViews[index].frame = newViewFrame
+                }
+        },
+            completion: { position in
+                UIView.transition(
+                    with: self.playingCardViews[index],
+                    duration: 0.75,
+                    options: .transitionFlipFromLeft,
+                    animations: {
+                        self.playingCardViews[index].backgroundColor = .blue
+                },
+                    completion: nil
+                )
+            }
+        )
+    }
+    
+    override func draw(_ rect: CGRect) {
+        gridSetup()
         switch animationState {
         case .begining:
             // setup
-            for card in playingCardViews {
-                card.frame.origin = dickFrame!.origin.offsetBy(dx: -self.frame.origin.x, dy: -self.frame.origin.y)
-                card.frame.size = dickFrame!.size
-                card.layer.cornerRadius = 6
-                //card.frame = dickFrame!
-                //card.bounds = dickFrame!
-                addSubview(card)
-            }
-            print("begining")
-            
             for index in 0..<playingCardViews.count {
-                UIViewPropertyAnimator.runningPropertyAnimator(
-                    withDuration: 0.75,
-                    delay: 0.375 * Double(index),
-                    options: .curveEaseInOut,
-                    animations: {
-                        if let viewFrame = grid[index] {
-                            let padding = viewFrame.height * Constants.paddingRatio
-                            let newViewFrame = viewFrame.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
-                            self.playingCardViews[index].frame = newViewFrame
-                        }
-                },
-                    completion: { position in
-                        UIView.transition(
-                            with: self.playingCardViews[index],
-                            duration: 0.75,
-                            options: .transitionFlipFromLeft,
-                            animations: {
-                                self.playingCardViews[index].backgroundColor = .blue
-                        },
-                            completion: nil
-                        )
-                }
-                )
+                playingCardViewSetup(index)
+            }
+            // animating
+            for index in 0..<playingCardViews.count {
+                deal3moreAnimation(index, with: Double(index))
             }
             
         case .changingBounds:
-            print("changingBounds")
-            
+            // layout
             for index in 0..<grid.cellCount {
-                if let viewFrame = grid[index] {
-                    let view = playingCardViews[index]
-                    let padding = viewFrame.height * Constants.paddingRatio
-                    let newViewFrame = viewFrame.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
-                    UIViewPropertyAnimator.runningPropertyAnimator(
-                        withDuration: 0.5,
-                        delay: 0,
-                        options: [.allowUserInteraction],
-                        animations: {
-                            view.frame = newViewFrame
-                    },
-                        completion: nil)
-                    view.frame = newViewFrame
-                    addSubview(view)
-                }
+                layoutNewsubviews(index)
             }
             
         case .dealing, .dealt:
+            // setup
             for index in playingCardViews.count-3..<playingCardViews.count {
-                playingCardViews[index].frame.origin = dickFrame!.origin.offsetBy(dx: -self.frame.origin.x, dy: -self.frame.origin.y)
-                playingCardViews[index].frame.size = dickFrame!.size
-                playingCardViews[index].layer.cornerRadius = 6
-                //card.frame = dickFrame!
-                //card.bounds = dickFrame!
-                addSubview(playingCardViews[index])
+                playingCardViewSetup(index)
             }
-            
+            // layout
             for index in 0..<grid.cellCount-3 {
-                if let viewFrame = grid[index] {
-                    let view = playingCardViews[index]
-                    let padding = viewFrame.height * Constants.paddingRatio
-                    let newViewFrame = viewFrame.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
-                    UIViewPropertyAnimator.runningPropertyAnimator(
-                        withDuration: 0.5,
-                        delay: 0,
-                        options: [.allowUserInteraction],
-                        animations: {
-                            view.frame = newViewFrame
-                    },
-                        completion: nil)
-                    addSubview(view)
-                }
+                 layoutNewsubviews(index)
             }
-            
-            // animating 3 more cards
-            
-            var delayAccelerator = 0
+            // animating
+            var delayFactor = 0
             for index in playingCardViews.count-3..<playingCardViews.count {
-                UIViewPropertyAnimator.runningPropertyAnimator(
-                    withDuration: 0.75,
-                    delay: 0.375 * Double(delayAccelerator),
-                    options: .curveEaseInOut,
-                    animations: {
-                        if let viewFrame = grid[index] {
-                            let padding = viewFrame.height * Constants.paddingRatio
-                            let newViewFrame = viewFrame.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
-                            self.playingCardViews[index].frame = newViewFrame
-                        }
-                },
-                    completion: { position in
-                        UIView.transition(
-                            with: self.playingCardViews[index],
-                            duration: 0.75,
-                            options: .transitionFlipFromLeft,
-                            animations: {
-                                self.playingCardViews[index].backgroundColor = .blue
-                        },
-                            completion: nil
-                        )
-                }
-                )
-                delayAccelerator += 1
+                deal3moreAnimation(index, with: Double(delayFactor))
+                delayFactor += 1
             }
         }
         animationState = .changingBounds
